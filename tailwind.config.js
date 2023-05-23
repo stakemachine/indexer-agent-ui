d65a98bf46fd35b42867666a665c9c5571ff5de2
stake-machine <80471159+stake-machine@users.noreply.github.com>
2023-05-23 13:35:25 +0300

    chore: watch react-daisyui cache

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,12 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}"
-    
+    "./node_modules/@tremor/**/*.{js,ts,jsx,tsx}",
+    "node_modules/daisyui/dist/**/*.js",
+    "node_modules/react-daisyui/dist/**/*.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
+};

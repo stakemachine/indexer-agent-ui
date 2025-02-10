e9669b1bbd0542eb255f9d1c097ab13892f43da6
stake-machine <80471159+stake-machine@users.noreply.github.com>
2025-02-10 18:42:00 +0300

    feat: authentication with NextAuth and credentials provider

--- middleware.ts
+++ middleware.ts
@@ -5,13 +5,16 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(function middleware(request: NextRequest) {
 	if (request.nextUrl.pathname.startsWith("/api/agent")) {
 		return NextResponse.rewrite(
-			new URL(process.env.AGENT_ENDPOINT, request.url),
+			new URL(process.env.AGENT_ENDPOINT as string, request.url),
 		);
 	}
 	if (request.nextUrl.pathname.startsWith("/api/subgraph")) {
 		const networkName = request.nextUrl.pathname.split("/")[3];
 		return NextResponse.rewrite(
-			new URL(process.env.AGENT_NETWORK_ENDPOINT + networkName, request.url),
+			new URL(
+				(process.env.AGENT_NETWORK_ENDPOINT as string) + networkName,
+				request.url,
+			),
 		);
 	}
 	return NextResponse.next();
